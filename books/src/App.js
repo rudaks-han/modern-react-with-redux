d464e9cb97fd5dd2519551437d815d52f82e7260
한경만 <kmhan@spectra.co.kr>
2024-02-23 07:36:20 +0900

    update

--- books/src/App.js
+++ books/src/App.js
@@ -1,39 +1,59 @@
 import './index.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
+import axios from 'axios';
 
 function App() {
 	const [books, setBooks] = useState([]);
 
-	const createBook = (title) => {
-		// BAD CODE
-		// books.push({ id: 123, title: title });
-		// setBooks(books);
-		setBooks((prevState) => {
-			return [...prevState, { id: Math.round(Math.random() * 9999), title }];
-		});
+	const fetchBooks = async () => {
+		const response = await axios.get('http://localhost:3001/books');
+		setBooks(response.data);
 	};
 
-	const editBookHandler = (id, newTitle) => {
+	const createBook = async (title) => {
+		const response = await axios.post('http://localhost:3001/books', {
+			title,
+		});
+
+		const updatedBooks = [...books, response.data];
+		setBooks(updatedBooks);
+		// setBooks((prevState) => {
+		// 	return [...prevState, { id: Math.round(Math.random() * 9999), title }];
+		// });
+	};
+
+	const editBookHandler = async (id, newTitle) => {
+		const response = await axios.put(`http://localhost:3001/books/${id}`, {
+			title: newTitle,
+		});
+
+		console.log('response', response);
+
 		const updatedBooks = books.map((book) => {
 			if (book.id === id) {
-				return {
-					...book,
-					title: newTitle,
-				};
+				console.log(response.data);
+				return { ...book, ...response.data };
 			}
 
 			return book;
 		});
+		console.log(updatedBooks);
 
 		setBooks(updatedBooks);
 	};
 
-	const deleteBookHandler = (id) => {
+	const deleteBookHandler = async (id) => {
+		await axios.delete(`http://localhost:3001/books/${id}`);
+
 		const updatedBooks = books.filter((book) => book.id !== id);
 		setBooks(updatedBooks);
 	};
+
+	useEffect(() => {
+		fetchBooks();
+	}, []);
 
 	return (
 		<div className="app">
