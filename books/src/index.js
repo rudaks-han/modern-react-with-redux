c0d3c6bb7f04f2f8d7dae23719e38e72649a2f38
rudaks <rudaks94@gmail.com>
2024-02-23 20:08:23 +0900

    update

--- books/src/index.js
+++ books/src/index.js
@@ -1,6 +1,12 @@
-import ReactDOM from "react-dom/client";
-import App from "./App";
-const el = document.querySelector("#root");
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { Provider } from './context/books';
+
+const el = document.querySelector('#root');
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(
+	<Provider>
+		<App />
+	</Provider>,
+);
